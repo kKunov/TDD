46dce1acd6f9e31a48b3d23bbad7a63386b887ac
Kaloian Kunov <kkunov121@gmail.com>
2015-05-28 13:09:32 +0300

    aaaaaaa

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,31 +38,45 @@ class NewVisitorTest(LiveServerTestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
+        # When she hits enter, she is taken to a new URL,
+        # and now the page lists: "1: Buy peacock feathers" as an item in a
+        # to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use the peacock feathers to make a fly')
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use the peacock feathers to make a fly'
             )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
+        # Now a new user, Francis, comes along to the site.
+        # # We use a new browser session to make sure that no information
+        # # of Edith's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # She visits that URL - her to-do list is still there.
+        # Francis visiting a new list by entering a neew item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
 
-        # Satisfied, she goes back to sleep
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
+
 
 if __name__ == '__main__':
-    LiveServerTestCase.main(warnings='ignore')
+    unittest.main(warnings='ignore')
